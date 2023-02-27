4e8ab13696cf48f96576cc6e98ff47aa9e697998
Lior Hai <liorhai@mta.ac.il>
2023-02-27 23:01:35 +0200

    pagerank code

--- pagerank.py
+++ pagerank.py
@@ -1,17 +1,19 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 import numpy as np
+import math
 from scipy.sparse.linalg import eigsh
 from scipy.sparse import csr_matrix
 from sklearn.preprocessing import normalize
 
-n = pow(2, 14)
+n = 10
 two_pow_13 =pow(2, 13)
 two_pow_12 =pow(2, 12)
 two_pow_11 =pow(2, 11)
 two_pow_10 =pow(2, 10)
 two_pow_9 =pow(2, 9)
 
+
+#graphs
 def normalize_mat(adjacency):
     # Calculate row sums
     row_sums = np.sum(adjacency, axis=1)
@@ -37,7 +39,7 @@ def clique(n):
     adjacency = nx.to_numpy_array(G)
     return G, adjacency
 
-cliqueG, adjacency_mat_clique= clique(two_pow_10)
+cliqueG, adjacency_mat_clique= clique(n )
 norm_mat_clique = normalize_mat(adjacency_mat_clique)
 print("finished creating clique")
 
@@ -81,9 +83,9 @@ def ring(n):
     adjacency = nx.to_numpy_array(G)
     return G, adjacency
 
-ringG, adjacency_mat_ring = ring(two_pow_10)
+ringG, adjacency_mat_ring = ring(n )
 norm_mat_ring = normalize_mat(adjacency_mat_ring)
-print("finished creating ring")
+#print("finished creating ring")
 
 #fig, ax = plt.subplots()
 #nx.draw(ringG, with_labels=True, ax=ax)
@@ -105,7 +107,7 @@ def candy(n):
     for i in range((n//2)+1, n+1):
         G.add_node(i)
         G.add_edge(i, i)
-        
+
 
     # Connect the clique vertices
     for i in range(n//2+1, n+1):
@@ -119,9 +121,9 @@ def candy(n):
     adjacency = nx.to_numpy_array(G)
     return G, adjacency
 
-candyG ,adjacency_mat_candy= candy(two_pow_10)
+candyG ,adjacency_mat_candy= candy(n )
 norm_mat_candy = normalize_mat(adjacency_mat_candy)
-print("finished creating candy")
+#print("finished creating candy")
 
 #fig, ax = plt.subplots()
 #nx.draw(candyG, with_labels=True, ax=ax)
@@ -151,24 +153,44 @@ def two_cliques(n):
         for j in range((3*n//4) +1, n+1):
             if i <= j:
                 G.add_edge(i, j)
-    
+
 
     G.add_edge(1, (n//2)+1)
     G.add_edge(n//2, (3*n//4) +1)
-    
+
     adjacency = nx.to_numpy_array(G)
     return G, adjacency
 
-two_cliques_G , adjacency_mat_two_cliques= two_cliques(two_pow_10)
+two_cliques_G , adjacency_mat_two_cliques= two_cliques(n )
 norm_mat_two_cliques = normalize_mat(adjacency_mat_two_cliques)
-print("finished creating two cliques")
+#print("finished creating two cliques")
 
 #fig, ax = plt.subplots()
 #nx.draw(two_cliques_G, with_labels=True, ax=ax)
 #ax.set_title("Two Cliques Graph")
 #plt.savefig("two_cliques_graph.png")
 
-#question 2
+
+def stationary_distribution(mat):
+    p = []
+    #adjacency = nx.to_numpy_array(G)
+    for row in mat:
+        p.append(sum(row))
+
+    p = np.array(p)
+    return p / p.sum()#, adjacency
+
+def normalize_mat(adjacency):
+    # Calculate row sums
+    row_sums = np.sum(adjacency, axis=1)
+
+    # Create diagonal matrix with inverse of row sums
+    D = np.diag(1 / np.sqrt(row_sums))
+
+    # Normalize adjacency matrix
+    normalized_adj_matrix = D.dot(adjacency)
+
+    return normalized_adj_matrix
 
 def stationary_distribution(mat):
     p = []
@@ -181,11 +203,11 @@ def stationary_distribution(mat):
 
 norm_mat_clique = normalize_mat(adjacency_mat_clique)
 stationary_clique = stationary_distribution(norm_mat_clique)
-print("stationary clique:", stationary_clique)
+#print("stationary clique:", stationary_clique)
 
 norm_mat_ring = normalize_mat(adjacency_mat_ring)
 stationary_ring = stationary_distribution(norm_mat_ring)
-print("stationary ring:", stationary_ring)
+#print("stationary ring:", stationary_ring)
 
 norm_mat_candy = normalize_mat(adjacency_mat_candy)
 stationary_candy = stationary_distribution(norm_mat_candy)
@@ -193,134 +215,86 @@ print("stationary candy:", stationary_candy)
 
 norm_mat_two_cliques = normalize_mat(adjacency_mat_two_cliques)
 stationary_two_cliques = stationary_distribution(norm_mat_two_cliques)
+#print("stationary two cliques:", stationary_two_cliques)
+
+'''
+adjacency_mat_clique ,stationary_clique = stationary_distribution(cliqueG)
+print("stationary clique:", stationary_clique)
+
+
+adjacency_mat_ring ,stationary_ring = stationary_distribution(ringG)
+print("stationary ring:", stationary_ring)
+
+
+adjacency_mat_candy ,stationary_candy = stationary_distribution(candyG)
+print("stationary candy:", stationary_candy)
+
+adjacency_mat_two_cliques, stationary_two_cliques = stationary_distribution(two_cliques_G)
 print("stationary two cliques:", stationary_two_cliques)
-#question 2/2
-
 '''
-def pagerank(M, num_iterations=100, d = pow(2,-6)):#d = 0.85
-    """
-    Implements the PageRank algorithm for computing the importance of web pages.
-    M: adjacency matrix where M[i][j] is 1 if there is a link from page j to page i, 0 otherwise
-    num_iterations: number of iterations to run the algorithm
-    d: damping factor
-    """
-    N = len(M)
-    v = np.random.rand(N, 1)
-    v = v / np.linalg.norm(v, 1)
-    M_hat = (d * M + (1 - d) / N)
-    for i in range(num_iterations):
-        v = M_hat @ v
-    return v
-    
-print(pagerank(adjacency_mat_ring))
-'''
-#question 3
 
- # Define power iteration method
-'''
-#in progress
-def power_iteration(A, v):
-    Av = np.dot(A,v)
-    v_new = Av / np.linalg.norm(Av)
-    return v_new
 
-def get_highest_eigenvalue(graph,A, tol):
-    # Initialize random vector
-    x = np.random.rand(len(graph))
-   
-    # Calculate adjacency matrix and normalize to obtain transition matrix
-    #D = np.diag(np.sum(A, axis=1))
-   # P = np.linalg.inv(D).dot(A)
 
-    lambda_1_old = 0
-    #old_x = x
-    while True:
-        x = power_iteration(A, x)
-        a = np.dot(x,A)
-        b = np.dot(a, x)
-        lambda_1 = b / np.dot(x,x)
+def random_walk(G, start_node, N, page_rank_dict, stat_vec, wanted_delta):
+    nodes = list(G.nodes())
+    visited = list()
+    current_node = start_node
+    steps = 0
+    while len(visited) < N:
+        visited.append(current_node)
+        steps += 1
+        if len(visited) == N-1:
+            page_rank_dict[current_node]+= 1
+        neighbors = list(G.neighbors(current_node))
+        current_node = np.random.choice(neighbors)
+    return steps
 
-        if abs(lambda_1 - lambda_1_old) < tol:
-       # if abs(np.linalg.norm(x-old_x)) < tol:
+
+
+def histogram(page_rank_dict, trails):
+    probability_vec = np.zeros(len(page_rank_dict))
+    for key, value in page_rank_dict.items():
+        probability_vec[key -1] = value / (trails +1)
+    return probability_vec
+
+
+def rand_walk_loop(G, n, stat_vec, wanted_delta, trials, N):
+    # when n is num of nodes in G, stat_vec is the matching vector
+    # trails mean t time pick a random node to start the walk from it
+    # Calculate the cover time for the uniform initial distribution
+    # N is the number of steps in each rand walking
+    cover_time = 0
+    page_rank_dict= {i: 0 for i in range(1, n+1)}
+    for i in range(trials):
+        rand_node = np.random.choice(list(G.nodes()))
+        cover_time += random_walk(G, rand_node, N, page_rank_dict, stat_vec, wanted_delta)
+        probability_vector = histogram(page_rank_dict, i)
+        curr_delta =stat_vec- probability_vector
+        curr_delta = np.linalg.norm( curr_delta)
+        #if i %10 == 0:
+            #print(curr_delta)
+        if curr_delta <= wanted_delta:
             break
-        lambda_1_old = lambda_1
-        #old_x = x
-        
-    return lambda_1, x
-
-def projection(u, v):
-    # calculate dot product of u and v
-        dot_uv = np.dot(u, v)
-
-    # calculate dot product of u with itself
-        dot_uu = np.dot(u, u)
-
-    # calculate projection of v onto u
-        proj_uv = (dot_uv / dot_uu) * u
-        return proj_uv
-
-def get_2nd_highest_eigenvalue(graph,A,u, tol):
-    w = np.random.rand(len(graph))
-    proj_uv = projection(u, w)
-    u0 = w - proj_uv
-
-    #D = np.diag(np.sum(A, axis=1))
-    #P = np.linalg.inv(D).dot(A)
-
-    #lambda_2_old = 0
-    old_u = u0
-    while True:#CRASHES FOR CLIQUE GRAPH because wt = Pu(wt) so the vt = 0
-        #to find new u
-        wt = np.dot(A, old_u)
-        vt = wt - projection(u, wt)
-        #print(np.linalg.norm(vt))
-        new_u = vt / np.linalg.norm(vt)
-
-        #to get the eigenvalue
-        a = np.dot(new_u,A)
-        b = np.dot(a, new_u)
-        lambda_2 = b / np.dot(new_u,new_u)
-        #if abs(lambda_2 - lambda_2_old) < tol:
-        if abs(np.linalg.norm(new_u - old_u)) < tol:
-            break
-        #lambda_2_old = lambda_2
-        old_u = new_u
-        
-    return lambda_2
+    cover_time /= trials
+    #print(probability_vector)
+    print(probability_vector)
+    print("the current delta is:")
+    print(curr_delta)
+    print("\n")
 
 
-def get_2_highest(G, A, tol):
-    lambda_1, x = get_highest_eigenvalue(G, A, tol)
-    lambda_2 = get_2nd_highest_eigenvalue(G, A, x, tol)
-    return lambda_1, lambda_2
+    for i in probability_vector:
+        print(i )
 
-print("for clique:")
-for k in range(2, 7):
-    tol = 2 ** -k
-    first, second = get_2_highest(cliqueG, norm_mat_clique, tol)
-    print(first, second)
-    print("Tolerance level:", tol, "Ratio of first and second eigenvalues:", first/second)
-    
-print("for ring:")
-for k in range(2, 7):
-    tol = 2 ** -k
-    first, second = get_2_highest(ringG, norm_mat_ring, tol)
-    print(first, second)
-    print("Tolerance level:", tol, "Ratio of first and second eigenvalues:", first/second)
-
-
-print("for candy:")
-for k in range(2, 7):
-    tol = 2 ** -k
-    first, second = get_2_highest(candyG, norm_mat_candy, tol)
-    print(first, second)
-    print("Tolerance level:", tol, "Ratio of first and second eigenvalues:", first/second)
-
-
-print("for two cliques:")
-for k in range(2, 7):
-    tol = 2 ** -k
-    first, second = get_2_highest(two_cliques_G, norm_mat_two_cliques, tol)
-    print(first, second)
-    print("Tolerance level:", tol, "Ratio of first and second eigenvalues:", first/second)
 '''
+    print("the sum of the vector is")
+    print(sum(probability_vector))
+    print("the average  of node in the vector is")
+    print(sum(probability_vector) / n)
+'''
+trials = 10000#try to do this function with while loop and count the trials it takes
+#round(n * math.log(n,2))
+N = pow(2,6)
+wanted_delta = pow(2, -6)
+print("page rank starting")
+rand_walk_loop(candyG, n, stationary_candy, wanted_delta, trials, N)
